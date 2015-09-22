4fcc076de5e29dc6e55081e73ef44acbe2e1e052
Tanghe Jerome <tanghe@a11p6.fil.univ-lille1.fr>
2015-09-22 09:49:24 +0200

    Création des classes currentmood.util.{Proxy,CMTwitter}

--- src/currentmood/util/CMTwitter.java
+++ src/currentmood/util/CMTwitter.java
@@ -1,5 +1,27 @@
 package currentmood.util;
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class CMTwitter {
+
+	protected final String TWITTER_PUBLIC_KEY = "",
+						   TWITTER_PRIVATE_KEY = "";
+	protected boolean authentified;
+	
+	protected Twitter twitterClient;
+	ConfigurationBuilder twitterConfiguration;
+	
+	public CMTwitter()
+	{
+		this.authentified = false;
+	}
+	
+	public void setProxy(Proxy proxy)
+	{
+		twitterConfiguration.setHttpProxyHost(proxy.getHost());
+		twitterConfiguration.setHttpProxyPort(proxy.getPort());
+		twitterConfiguration.setHttpProxyUser(proxy.getUser());
+		twitterConfiguration.setHttpProxyPassword(proxy.getPassword());
+	}
 
 }
