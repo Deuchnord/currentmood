be6a07e98a1d5e396818fd7570332df56632d962
qvandekadsye <qvandekadsye@gmail.com>
2015-11-24 13:22:25 +0100

    le graphique s'affiche

--- src/currentmood/UI/WinChart.java
+++ src/currentmood/UI/WinChart.java
@@ -4,10 +4,16 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import org.jfree.chart.ChartPanel;
 
 import currentmood.UI.chart.PieChart;
+
+import org.jfree.chart.*;
+import org.jfree.chart.plot.*;
+import org.jfree.data.*;
+import org.jfree.data.general.*;
 
 public class WinChart extends JFrame {
 
@@ -15,19 +21,37 @@ public class WinChart extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 6264810561528922939L;
-	protected PieChart chart;
+	private DefaultPieDataset dataset = new DefaultPieDataset();
+	private JFreeChart chart;
+	private ChartPanel cp;
+	private JPanel panelw;
+	
 	
 	public WinChart(PieChart panel ,int negatives, int neutrals, int positives)
 	{
-		this.chart = panel;
+		panelw = new JPanel();
+		
+		dataset.setValue("Mauvais", negatives);
+		dataset.setValue("Neutre", neutrals);
+		dataset.setValue("Bon", positives);
+		chart = ChartFactory.createPieChart(
+				"Répartion des sentiments", 
+				dataset,
+				true, 
+				true, 
+				false);
+		
+		this.cp=new ChartPanel(chart);
+		this.add(this.cp);
 		this.setTitle("Résultat de la classification");
 		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 		//Paramétrage de la fenetre 
 		this.setSize(new Dimension((int)screen.getWidth()/2,(int)screen.getHeight()/2));
-		ChartPanel cp = chart.createChart(negatives, neutrals, positives);
-		this.add(cp);
 		this.setVisible(true);
-		cp.validate();
+		
+		
+		
+		
 		
 		
 	}
